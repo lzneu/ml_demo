6f27de40587927fa367e515de701dbd6db6befe3
lz867 <lizhuangneu@163.com>
2018-05-06 22:07:00 +0800

    增加adaboost

---
+++ adaboost.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : adaboost.py
+# @Author: 投笔从容
+# @Date  : 2018/5/6
+# @Desc  : 手写实现adaboost
+
+from numpy import *
+
+
+
+if __name__ == '__main__':
+    
